4cc6505665e0f0f8eb8d9253acac29a76a4e7193
Shrimp <micro-derk@yandex.ru>
2025-02-23 19:08:31 +0500

    Add files via upload

---
+++ dz/dz_20_02_25.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int summ(int a, int b)
+{
+	return a + b;
+}
+int i_forgor_the_word(int a, int b)
+{
+	return a * b;
+}
+int main()
+{
+	summ(10, 10);
+	//i must have dementia because i genuenly don't remember how to do them and study materials or whatever tf it's called aren't helping
+	i_forgor_the_word(10, 20);
+	return 0;
+}
