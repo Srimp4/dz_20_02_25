e5e9b5ca5979f53df684b038e406bbd28bce0bdb
Shrimp <micro-derk@yandex.ru>
2025-03-24 14:08:07 +0500

    Update dz_20_02_25.cpp

--- dz/dz_20_02_25.cpp
+++ dz/dz_20_02_25.cpp
@@ -1,17 +1,32 @@
-﻿#include <iostream>
-using namespace std;
-int summ(int a, int b)
+#include <iostream>
+int sum(int a, int b)
 {
-	return a + b;
+    if (a < 0) {
+        if (a == 0) return a;
+        else return sum(a + 1, b - 1);
+    }
+    if (a == 0) return b;
+    else return sum(a - 1, b + 1);
 }
-int i_forgor_the_word(int a, int b)
-{
-	return a * b;
+int multiply(int a, int b) {
+    if (b == 0) {
+        return 0;
+    }
+    else {
+        return a + multiply(a, b - 1);
+    }
 }
+
 int main()
 {
-	summ(10, 10);
-	//i must have dementia because i genuenly don't remember how to do them and study materials or whatever tf it's called aren't helping
-	i_forgor_the_word(10, 20);
-	return 0;
+    int x; int y;
+    std::cin >> x >> y;
+    std::cout << sum(x, y);
+    //i'm stoopid
+    int a = 5;
+    int b = 3;
+    int result = multiply(a, b);
+    std::cout << result;
+
+    return 0;
 }
